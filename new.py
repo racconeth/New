7820124e38557f94e6dcb16f9d3bd7e69ed3c1bd
raccon.eth <26019596+racconeth@users.noreply.github.com>
2022-12-12 13:53:03 -0600

    Update new.py

--- new.py
+++ new.py
@@ -1,21 +1,13 @@
 import random
 
-# Function to generate a random phone number
-def generate_phone_number():
-  # Randomize the first three digits
-  first_digits = str(random.randint(100, 999))
-  
-  # Randomize the last seven digits
-  last_digits = str(random.randint(1000000, 9999999))
-  
-  # Return the phone number in the format "XXX-YYYYYYY"
-  return first_digits + "-" + last_digits
-  
-# Generate 1000 phone numbers
-phone_numbers = []
-for i in range(1000):
-  phone_number = generate_phone_number()
-  phone_numbers.append(phone_number)
-  
-# Print the first 10 phone numbers
-print(phone_numbers[:10])
+# Start with the base number
+number = "+1201850"
+
+# Generate 4 random numbers
+for i in range(4):
+  # Append a random number to the base number
+  number += str(random.randint(0, 9))
+
+# Save the resulting number to a text file
+with open("numbers.txt", "w") as f:
+  f.write(number)
