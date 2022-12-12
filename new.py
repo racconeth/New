4ab009323cc6b3fe34c6fb6aa7a345e8f8db8afe
raccon.eth <26019596+racconeth@users.noreply.github.com>
2022-12-12 13:49:13 -0600

    Update new.py

--- new.py
+++ new.py
@@ -1,11 +1,21 @@
 import random
 
-# Generate random phone number
-area_code = random.randint(100, 999)
-first_three = random.randint(100, 999)
-last_four = random.randint(1000, 9999)
-
-phone_number = f"({area_code}) {first_three}-{last_four}"
-
-# Print the phone number
-print(phone_number)
+# Function to generate a random phone number
+def generate_phone_number():
+  # Randomize the first three digits
+  first_digits = str(random.randint(100, 999))
+  
+  # Randomize the last seven digits
+  last_digits = str(random.randint(1000000, 9999999))
+  
+  # Return the phone number in the format "XXX-YYYYYYY"
+  return first_digits + "-" + last_digits
+  
+# Generate 1000 phone numbers
+phone_numbers = []
+for i in range(1000):
+  phone_number = generate_phone_number()
+  phone_numbers.append(phone_number)
+  
+# Print the first 10 phone numbers
+print(phone_numbers[:10])
